c6f5483dc1dfd7d3e7d26837071cf68dd4e126f6
Julian Giamblanco <giamblanco.julian@gmail.com>
2020-11-11 17:46:54 -0800

    Add database

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-use rocket::http::RawStr;
+use rocket::{http::{RawStr, uri::Uri}, response::Redirect};
 use diesel::PgConnection;
+pub mod schema;
+pub mod models;
+use self::models::*;
+
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate diesel;
+
 
 #[database("diesel_postgres")]
 struct PgDbConn(PgConnection);
 
 
 #[get("/<greeting>/<person>")]
-fn index(person: &RawStr, greeting: &RawStr) -> String {
+fn index(person: String, greeting: String) -> String {
     format!("{} {}!", greeting.as_str(), person.as_str())
 }
 
+#[get("/")]
+fn home() -> Redirect {
+    let greeting: &str = "Hello";
+    let person: &str = "stranger";
+    Redirect::to(uri!(index: person = String::from(person), greeting = String::from(greeting)))
+}
+
 fn main() {
-    rocket::ignite().attach(PgDbConn::fairing()).mount("/", routes![index]).launch();
+    rocket::ignite()
+    .attach(PgDbConn::fairing())
+    .mount("/", routes![index, home])
+    .launch();
 }
